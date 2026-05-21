ad5e0d41c0ee76876eea4dc6202bf19c3f57b36c
Krzysztof Franczyk <krzysztofranczyk@gmailcom>
2026-05-21 15:49:04 +0200

    deskop-mobile--menu-bugs-ok.1.2

---
+++ HouseNet9/wwwroot/js/menu.js
@@ -0,0 +1,44 @@
+﻿
+//document.addEventListener("DOMContentLoaded", function () {
+
+//    const nav = document.getElementById("mainNav");
+//    const collapse = document.getElementById("navbarResponsive");
+
+//    collapse.addEventListener("show.bs.collapse", function () {
+//        nav.classList.add("menu-open");
+//    });
+
+//    collapse.addEventListener("hide.bs.collapse", function () {
+//        nav.classList.remove("menu-open");
+//    });
+
+//});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const nav = document.getElementById("mainNav");
+    const collapse = document.getElementById("navbarResponsive");
+    const toggler = document.querySelector(".custom-toggler");
+
+    // OPEN MENU
+    collapse.addEventListener("show.bs.collapse", function () {
+        nav.classList.add("menu-open");
+        toggler.classList.add("active");
+    });
+
+    // CLOSE MENU
+    collapse.addEventListener("hide.bs.collapse", function () {
+        nav.classList.remove("menu-open");
+        toggler.classList.remove("active");
+    });
+
+    // SCROLL EFFECT
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 10) {
+            nav.classList.add("scrolled");
+        } else {
+            nav.classList.remove("scrolled");
+        }
+    });
+
+});
