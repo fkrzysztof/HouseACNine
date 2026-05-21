2765d7b6b398dce530ee1da4c6b2da4e2c7577ef
Krzysztof Franczyk <krzysztofranczyk@gmailcom>
2026-05-21 13:08:40 +0200

    deskop-mobile-bugs-ok.1.1

---
+++ HouseNet9/wwwroot/js/backToTop.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const btn = document.getElementById("backToTop");
+
+    if (!btn) return;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            btn.classList.add("show");
+        } else {
+            btn.classList.remove("show");
+        }
+    });
+
+    btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+});
---
+++ HouseNet9/wwwroot/js/ratingBox.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+
+    const overlay = document.getElementById('ratingOverlay');
+    const inline = document.getElementById('ratingInline');
+
+    window.addEventListener('scroll', () => {
+
+        const trigger = window.innerHeight * 0.5;
+
+        if (window.scrollY > trigger) {
+            overlay.style.opacity = "0";
+            overlay.style.pointerEvents = "none";
+
+            inline.classList.remove('hidden');
+        } else {
+            overlay.style.opacity = "1";
+            overlay.style.pointerEvents = "auto";
+
+            inline.classList.add('hidden');
+        }
+    });
+
+});
